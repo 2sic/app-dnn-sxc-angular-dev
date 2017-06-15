b6ecb38ec14e09e1bf93983f300c708bbc0501ad
2SIC\2cb <christoph.buehler@2sic.com>
2017-06-15 14:18:11 +0200

    added readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { SxcAppComponent } from "@2sic.com/sxc-angular";
+import { SxcAppComponent } from "@2sic.com/dnn-sxc-angular";
 
 @Component({
-  selector: 'app-root',
-  template: `this is a test`,
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    template: `this is a test`,
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent extends SxcAppComponent {
 }
