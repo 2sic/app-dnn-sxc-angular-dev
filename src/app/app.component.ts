9cafb957a60ae0efaf8c8af06ccb386e90909f0b
Raphael Müller <raaphii@gmail.com>
2018-04-05 08:54:34 +0200

    Removed unneeded import and text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
 import { Context, Data, DnnAppComponent } from '@2sic.com/dnn-sxc-angular';
-import { SxcTestInterface } from '@2sic.com/2sxc-typings';
 
 @Component({
     selector: 'app-root',
-    template: `WITH TYPINGS!: <app-person *ngFor="let person of persons" [person]="person"></app-person>`,
+    template: `List of all persons: <app-person *ngFor="let person of persons" [person]="person"></app-person>`,
     styleUrls: ['./app.component.css']
 })
 export class AppComponent extends DnnAppComponent implements OnInit {
@@ -16,9 +15,6 @@ export class AppComponent extends DnnAppComponent implements OnInit {
         private data: Data,
     ) {
         super(el, context);
-        const a: SxcTestInterface = {
-          name: 'lorem'
-        };
     }
 
     ngOnInit() {
